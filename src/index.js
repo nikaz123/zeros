387a612b78b46cb6f5ec5ce351acc8e1db431eb1
Lenovo <nikaz@tut.by>
2018-02-27 23:34:43 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+    let countZero = 0;
+
+
+    for (let i = 5; i <= number; i = i + 5) {
+
+        let j = 0;
+        let k = 5;
+        let ii = i;
+        do {
+            if (ii % k === 0) // "x" - Число, которое нам требуется разложить. Должно задаваться заранее.
+            {
+
+                j++;
+                ii = ii / k;
+            }
+
+        }
+
+        while (ii % k === 0);
+        countZero = countZero + j;
+
+    }
+
+
+    return (countZero);
+
+
 }
+
+
+
